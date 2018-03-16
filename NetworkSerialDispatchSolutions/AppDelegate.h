629c96b7c0238d59469374a7019cc09a8b038585
ralph <inexcii@gmail.com>
2018-03-16 23:55:33 +0900

    Initial Commit

---
+++ NetworkSerialDispatchSolutions/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetworkSerialDispatchSolutions
+//
+//  Created by Ralph Zhou on 2018/03/16.
+//  Copyright © 2018 Genmis Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
